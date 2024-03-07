233a336faa23e347e642580296092a531cc79212
Slowgun <betman321@gmail.com>
2024-03-07 19:24:40 +0900

    dd

---
+++ 2week Lv1 Repot.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+// Lv1
+
+class Calculator {
+    func calculate(oper: String, firstNumber: Double, secondNumber: Double) -> Double {
+        if oper == "+" {return firstNumber + secondNumber
+        } else if oper == "-" {return firstNumber - secondNumber
+        } else if oper == "*" {return firstNumber * secondNumber
+        } else if oper == "/" {return firstNumber / secondNumber
+        } else {
+            return  0
+        }
+    }
+}
+
+let calculator = Calculator()
+let addResult = calculator.calculate(oper: "+", firstNumber: 7, secondNumber: 6)
+let subtractResult = calculator.calculate(oper: "-", firstNumber: 8, secondNumber: 5)
+let multiplyResult = calculator.calculate(oper: "*", firstNumber: 2, secondNumber: 9)
+let divideResult = calculator.calculate(oper: "/", firstNumber: 15, secondNumber: 4)
+
+print("addResult : \(addResult)")
+print("subtractResult : \(subtractResult)")
+print("multiplyResult : \(multiplyResult)")
+print("divideResult : \(divideResult)")
